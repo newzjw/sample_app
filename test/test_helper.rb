382a06b7c7c23614257f05abd6ce9f98100f13a0
newzjw <newzjw713@163.com>
2018-10-09 16:55:44 +0800

    Complete advanced setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require "minitest/reporters"  #新加 配置测试，显示红色和绿色
+Minitest::Reporters.use!     #新加
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
